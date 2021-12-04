8b0a837427c8a3e991d4aed74ee3b5a2f293ccaa
djszklo <47299187+djszklo@users.noreply.github.com>
2021-12-04 22:37:48 +0100

    -

--- graphs.js
+++ graphs.js
@@ -1,4 +1,5 @@
 const graph = document.getElementById('graph');
+const dateTime = document.getElementById('date');
 
 (async()=>{
     graph.innerHTML="";
@@ -7,18 +8,23 @@ const graph = document.getElementById('graph');
     const {min,max} = findMinMax(data);
     const dataWithIndex = data.map((el,idx)=>[el,idx]);
     for (const [priceAndDate,index] of dataWithIndex) {
+        
         const element = document.createElement('div');
-        //element.innerText = `${priceAndDate.data}${priceAndDate.cena}`;
+        element.innerText = `${priceAndDate.cena}`;
         //element.style.minHeight = `${priceAndDate.cena / graphHeight * 100}%`;
         //element.style.height = `${priceAndDate.cena / graphHeight * 100}%`;
         const child = document.createElement("div");
         child.style.width = "80%";
         child.style.flexBasis = `${(priceAndDate.cena - min + 10) / (max + 20) * 500}%`; //"50%";
         child.style.backgroundColor = `rgba(65, 6, 173, ${index / 60 + 0.5})`;
+        
+
         element.appendChild(child);
         graph.appendChild(element);
-
+        
+        
     }
+    dateTime.innerText = `${data[0].data} - ${data[data.length-1].data}`
 })()
 
 function findMinMax(data){
